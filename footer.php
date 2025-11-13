4f5b353fbb07d074742bab92dcd7f67ac9527cba
Yfufri <theolannier50@gmail.com>
2025-11-13 17:38:24 +0100

    creation base du header et reduction du footer

--- footer.php
+++ footer.php
@@ -1,38 +1,4 @@
-<?php
-use Dotenv\Dotenv;
-require_once 'vendor/autoload.php';
 
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-var_dump($_ENV);
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Home</title>
-        <link href="./style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <h1>HELLO WORLD</h1>
-        
-    </body>
-</html>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
 <footer>
     <p>© 2025 Musicode · IUT Laval - R3.01 Développement web 2025-2026.</p>
 </footer>
-</html>
--- index.php
+++ index.php
@@ -5,7 +5,6 @@ require_once 'vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-var_dump($_ENV);
 
 ?>
 
@@ -16,10 +15,14 @@ var_dump($_ENV);
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Home</title>
         <link href="./style.css" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     </head>
     <body>
         <?php require "./header.php"?>
         <h1>HELLO THEO</h1>
-        
+        <?php var_dump($_ENV); ?>
+        <?php require "./footer.php"?>
     </body>
 </html>
