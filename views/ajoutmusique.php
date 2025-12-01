a227fa883fba4f53ff1cc54b4c46dddbd5f97f57
Hugo <hugobard35@gmail.com>
2025-12-01 16:28:42 +0100

    modif ajoutmusique

--- views/ajoutmusique.php
+++ views/ajoutmusique.php
@@ -1,19 +1,19 @@
-<a href="/catalogue">⬅️ Retour au catalogue</a><br><br>
-<h1>Ajouter une Musique</h1>
+<a href="/catalogue">⬅️ Retour au catalogue</a>
+<h2>Ajouter une Musique</h2>
 <a>Complétez les informations ci-dessous pour publier un nouveau morceau dans le catalogue.</a>
 <form action="/ajoutmusique" method="POST" enctype="multipart/form-data">
     <label for="title">Titre *</label>
-    <input type=" " id="title" name="title" required><br><br>
+    <input type=" " id="title" name="title" required>
 
     <label for="artist">Artiste *</label>
-    <input type=" " id="artist" name="artist" required><br><br>
+    <input type=" " id="artist" name="artist" required>
 
     <label for="album">Album </label>
-    <input type="Optionnel " id="album" name="album"><br><br>
+    <input type="Optionnel " id="album" name="album">
 
-    <label for="genre">Durée *</label><br>
+    <label for="genre">Durée *</label>
     <input type1="Minutes " id="duration" name="duration"> <t>: </t>
-    <input type2="Secondes " id="duration" name="duration"><br><br>
+    <input type2="Secondes " id="duration" name="duration">
 
     <input type="submit" value="Enregistrer la Musique">
 </form>
