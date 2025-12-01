3e810f803a304dccb05e696c4d8bc528bd805128
Kenan <kenan.guerin.etu@univ-lemans.fr>
2025-12-01 16:28:43 +0100

    edit moncompte.php

---
+++ views/moncompte.php
@@ -0,0 +1,26 @@
+<section>
+    <h3>Mon compte</h2>
+    <div class="monCompte">
+        <form action="" method="POST">
+            <div class>
+                <label for="nomAff" >Nom d'affichage</label>
+                <input type="text" id="nomAff" name="nomAff" required>
+            </div>
+            <div class="inputMdp">
+                <div class="filsInputMdp">
+                    <label for="nouveauMdp">Nouveau mot de passe</label>
+                    <input type="password" id="nouveauMdp" name="nouveauMdp">
+                    <p>Laissez vide pour conserver l'actuel</p>
+                </div>
+                <div class="filsInputMdp">
+                    <label for="confirmMdp">Confirmation</label>
+                    <input type="password" id="confirmMdp" name="confirmMdp">
+                </div>
+            </div>
+
+            <div class="btnMettreAJourMDP">
+                <input type="submit" value="Mettre à jour">
+            </div>
+        </form> 
+    </div>
+</section> 
